80fa90c90c0be4b42567e2e1fa58c3f8f908a164
Valentin <valentinarpin11@live.fr>
2023-07-24 22:29:50 +0200

    fix(#38)add event route

---
+++ routes/events.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {createEvent, getEvents} from "../controllers/events.js";
+
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/", verifyToken, getEvents);
+
+export default router;
