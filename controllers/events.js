3e6c5060beda66e3ac9bac49241853dc87f2b66d
Valentin <valentinarpin11@live.fr>
2023-07-24 22:24:29 +0200

    fix(#38)add getEvent

--- controllers/events.js
+++ controllers/events.js
@@ -1,51 +1,32 @@
 import Event  from "../models/Event.js";
 import User from "../models/User.js";
 
-    export const createEvent = async (req, res) => {
-        try {
-            const { userId, title, description, date, picturePath } = req.body;
+/*CREATE EVENT*/
+export const createEvent = async (req, res) => {
+    try {
+        const { userId, title, description, date, picturePath } = req.body;
 
-            const newEvent = new Event({
-                userId: userId,
-                title: title,
-                description: description,
-                date: date,
-                picturePath: picturePath
-            });
-            await newEvent.save();
+        const newEvent = new Event({
+            userId: userId,
+            title: title,
+            description: description,
+            date: date,
+            picturePath: picturePath
+        });
+        await newEvent.save();
 
-            res.status(201).json(newEvent);
-        } catch (err) {
-            res.status(409).json({ message: err.message });
-        }
+        res.status(201).json(newEvent);
+    } catch (err) {
+        res.status(409).json({ message: err.message });
     }
+}
 
-    /*READ EVENTS*/
-    export const getEvents = async (req, res) => {
-        try {
-            const events = await Event.find();
-            res.status(200).json(events);
-        } catch (err) {
-            res.status(404).json({ message: err.message });
-        }
+/*READ EVENTS*/
+export const getEvents = async (req, res) => {
+    try {
+        const events = await Event.find();
+        res.status(200).json(events);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
     }
-
-    export const createEvent = async (req, res) => {
-        try {
-            const { userId, title, description, date, picturePath } = req.body;
-
-            const newEvent = new Event({
-                userId: userId,
-                title: title,
-                description: description,
-                date: date,
-                picturePath: picturePath
-            });
-            await newEvent.save();
-
-            res.status(201).json(newEvent);
-        } catch (err) {
-            res.status(409).json({ message: err.message });
-        }
-    }
-
+}
