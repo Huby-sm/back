bf38874658f7b3fdebcc127cfe573302b065d3e6
Valentin <valentinarpin11@live.fr>
2023-07-24 21:13:00 +0200

    fix(#38)add event model

--- models/Event.js
+++ models/Event.js
@@ -3,12 +3,6 @@ import { Schema } from "mongoose";
 
 const EventSchema = new mongoose.Schema(
     {
-        userId: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-
         title: {
             type: String,
             required: true,
