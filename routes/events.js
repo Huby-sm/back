dc69fdeed1c128b1a88046e52d61abbf0128ae68
H4ltor <timothee.cognard@gmail.com>
2023-09-10 18:04:17 +0200

    fix(#71)add image event s3

--- routes/events.js
+++ routes/events.js
@@ -2,14 +2,14 @@ import express from "express";
 import {createEvent, getEvents} from "../controllers/events.js";
 
 import { verifyToken } from "../middleware/auth.js";
-import { isBde} from "../middleware/bde.js";
+//import { isBde} from "../middleware/bde.js";
 import { isAdmin } from "../middleware/admin.js";
 
 const router = express.Router();
 
 /* CREATE */
-router.post("/", [verifyToken, isAdmin], createEvent);
-router.post("/", [verifyToken, isBde], createEvent);
+//router.post("/", [verifyToken, isAdmin], createEvent);
+//router.post("/", [verifyToken, isBde], createEvent);
 
 /* READ */
 router.get("/", verifyToken, getEvents);
